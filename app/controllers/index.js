745624fdbea6110b9afa5d8c66be31d012e79c4f
Irina Chichikova <diva.artim@gmail.com>
2015-05-27 21:01:07 +0300

    Note adding form

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	actions: {
+		doneAdding: function() {
+			var note = this.store.createRecord('note', {
+				id: new Date().getTime(),
+				title: this.get('title'),
+				date: new Date()
+			});
+			note.save();
+			console.log('The note "' + this.get('title') + '" was created');
+			this.set('title', '');
+		}
+	}
 });
