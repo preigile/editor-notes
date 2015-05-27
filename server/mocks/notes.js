b6982187122c451809ba01c5825329bee9b79fde
Irina Chichikova <diva.artim@gmail.com>
2015-05-27 20:54:47 +0300

    Mocks and fixtures

--- server/mocks/notes.js
+++ server/mocks/notes.js
@@ -4,7 +4,28 @@ module.exports = function(app) {
 
   notesRouter.get('/', function(req, res) {
     res.send({
-      'notes': []
+      'notes': [
+        {
+          id: 1,
+          title: 'Какой чудесный день',
+          date: Date.now() - (1000 * 60 * 60 * 24)
+        },
+        {
+          id: 2,
+          title: 'Какой чудесный пень',
+          date: new Date(2013, 7, 1, 8, 3, 0)
+        },
+        {
+          id: 3,
+          title: 'Какой чудесный я',
+          date: new Date(2015, 4, 27, 6, 8, 0)
+        },
+        {
+          id: 4,
+          title: 'И песенка моя',
+          date: new Date(2015, 2, 1, 6, 5, 0)
+        }
+      ]
     });
   });
 
