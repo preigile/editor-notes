289f79b775602e9f720f33d2d96a82f3d2e59aa4
Irina Chichikova <diva.artim@gmail.com>
2015-05-28 09:54:31 +0300

    Note editing and deleting form

--- app/controllers/note.js
+++ app/controllers/note.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	isEditing: false,
+	actions: {
+		edit: function() {
+			this.set('isEditing', true);
+		},
+		doneEditing: function() {
+			this.set('isEditing', false);
+			this.store.push('note', {
+				id: this.get('model.id'),
+				title: this.get('model.title')
+			}).save();
+			console.log('The Note "' + this.get('model.title') + '" was edited');
+		},
+		removeNote: function() {
+			var note = this.get('model');
+			note.destroyRecord();
+			console.log('The Note "' + this.get('model.title') + '" was removed');
+		}
+	}
 });
