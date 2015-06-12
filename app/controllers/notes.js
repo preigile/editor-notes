7677fb0a5b9623bd1ab2750529b50e5a78735726
Irina Chichikova <diva.artim@gmail.com>
2015-06-12 11:34:59 +0300

    Note deleting

--- app/controllers/notes.js
+++ app/controllers/notes.js
@@ -1,15 +1,23 @@
 import Ember from 'ember';
 
-export default Ember.ArrayController.extend({
+export default Ember.Controller.extend({
+	
 	noteMessage: function() {
-		var notesCount = this.get('model.length');
-		if (notesCount === 1) {
-			return 'You have ' + notesCount + ' note';
-		} else { 
-			return 'You have ' + notesCount + ' notes';
-		}
-	}.property('model.length'),
+		var userId = this.get('model.id');
+		var notesCount = this.get('model.notes.length');
 
-	sortProperties: ['date'],		
-	sortAscending: false
+		if (notesCount === 1) return 'You have ' + notesCount + ' note';
+		if (notesCount > 1) return 'You have ' + notesCount + ' notes';
+
+		this.transitionToRoute('user', userId);
+	}.property('model.notes.length'),
+
+	sortProperties: ['date:desc'],
+	sortedNotes: Ember.computed.sort('model.notes', 'sortProperties'),
+
+	actions: {
+		removeNote: function(note) {
+			note.destroyRecord();
+		}
+	}
 });
