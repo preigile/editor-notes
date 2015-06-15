153d206d5c303a6b96dcd40f437490a3d371d4ed
Irina Chichikova <diva.artim@gmail.com>
2015-06-15 21:50:11 +0300

    Some fix

--- app/controllers/notes.js
+++ app/controllers/notes.js
@@ -3,13 +3,12 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 	
 	noteMessage: function() {
-		var userId = this.get('model.id');
 		var notesCount = this.get('model.notes.length');
 
 		if (notesCount === 1) { return 'You have ' + notesCount + ' note'; }
 		if (notesCount > 1) { return 'You have ' + notesCount + ' notes'; }
 
-		this.transitionToRoute('user', userId);
+		this.transitionToRoute('index');
 	}.property('model.notes.length'),
 
 	sortProperties: ['date:desc'],
