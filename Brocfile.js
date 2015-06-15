a5535ba5b2d1b8d03905bc4cf41262b0b2ae268a
Irina Chichikova <diva.artim@gmail.com>
2015-06-15 10:55:46 +0300

    Redundant

--- Brocfile.js
+++ Brocfile.js
@@ -12,19 +12,6 @@ var extraAssets = pickFiles('bower_components/bootstrap/dist/fonts',{
     destDir: '/fonts'
 });
 
-// Use `app.import` to add additional libraries to the generated
-// output files.
-//
-// If you need to use different assets in different
-// environments, specify an object as the first parameter. That
-// object's keys should be the environment name and the values
-// should be the asset to use in that environment.
-//
-// If the library that you are including contains AMD or ES6
-// modules that you would like to import into your application
-// please specify an object with the list of modules as keys
-// along with the exports of each module as its value.
-
 app.import('bower_components/bootstrap/dist/js/bootstrap.js');
 app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
 app.import('bower_components/moment/min/moment.min.js');
