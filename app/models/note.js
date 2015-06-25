fd70eabc6f6664f0df25c946d3c93aa951a0865c
Irina Chichikova <diva.artim@gmail.com>
2015-06-25 20:04:59 +0300

    Dynamic time

--- app/models/note.js
+++ app/models/note.js
@@ -5,5 +5,9 @@ var attr = DS.attr;
 export default DS.Model.extend({
 	title: attr('string'),
 	date: attr('date'),
-	user: DS.belongsTo('user')
+	user: DS.belongsTo('user'),
+	didLoad: function(){
+		var self = this;
+		setInterval(function() { self.reload() }, 1 * 60 * 1000);
+	}
 });
